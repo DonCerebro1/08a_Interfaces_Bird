10035e70ae5e5a985b9bc5466798b10598429d00
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-12-10 10:12:09 +0100

    added Interfaces

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -3,8 +3,15 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+        Bird bird  = new Bird();
+        ausgabe(bird.fly());
+        ausgabe(bird.hasFeathers());
 
     
     }
+
+    private static void ausgabe(String outStr) {
+        System.out.println(outStr);
+        }
 
 }
---
+++ src/com/cc/java/Bird.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Bird implements Flyable,IFeathers {
+
+    @Override
+    public String fly() {
+        return " I can fly";
+    }
+
+    @Override
+    public String hasFeathers(){
+        return " I am a bird, I have Feathers";
+    }
+    
+}
